36ee84ffca0c98c6b0fc0ce21f84516550a1e227
Stennnnix <37344868+Stennnnix@users.noreply.github.com>
2018-04-10 19:18:15 +0200

    Cleaned up

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,7 +25,7 @@ export default class HomeScreen extends React.Component {
             <Image
               source={
                 __DEV__
-                  ? require('../assets/images/logo.png')
+                  ? require('../assets/images/reallogo.png')
                   : require('../assets/images/robot-prod.png')
               }
               style={styles.logoImage}
@@ -33,31 +33,15 @@ export default class HomeScreen extends React.Component {
           </View>
 
           <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
+            <Text style={styles.getStartedText}>The only app...</Text>
 
             <Text style={styles.getStartedText}>
-              Hello Word.
+              (note to self: insert button here)
             </Text>
           </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
+          
         </ScrollView>
 
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
       </View>
     );
   }
@@ -125,7 +109,7 @@ const styles = StyleSheet.create({
   },
 
   logoImage: {
-    width: 175,
+    width: 250,
     height: 175,
     resizeMode: 'contain',
     marginTop: 3,
